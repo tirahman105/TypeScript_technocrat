0ff5d0bcd6679f2f99b7d25d1350b1415753dd0d
tirahman105 <tirahman105@gmail.com>
2023-11-05 20:48:09 +0600

    promise

---
+++ module2/2.8.ts
@@ -0,0 +1,58 @@
+{
+    // 
+
+// promise 
+type Todo = {
+    id: number;
+    userId: number;
+    title: string;
+    completed: boolean
+}
+
+const getTodo =async () : Promise<Todo> => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const data = await response.json();
+    return data
+    // console.log(data)
+};
+
+getTodo();
+
+
+
+
+type Something = {something :string} 
+
+// simulate
+const createPromise = () : Promise<Something> => {
+    return new Promise <Something>((resolve, reject) => {
+        const data : Something = 'something '
+        if(data){
+            resolve(data)
+        }else{
+            reject('failed to load data')
+        }
+    })
+}
+
+// calling createPromise function 
+
+const showData = async() : Promise<Something> => {
+    const data :Something = await createPromise()
+    return data
+    console.log(data)
+}
+
+showData();
+
+
+
+
+
+
+
+
+
+
+    // 
+}
